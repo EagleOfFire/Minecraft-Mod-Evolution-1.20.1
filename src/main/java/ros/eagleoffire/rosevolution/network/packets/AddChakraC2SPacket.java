36c02da9ff48634adba42f32a7a4f6823d8dc7f1
EagleOfFire <maxoucrafteur83@gmail.com>
2024-12-16 19:27:09 +0100

    ajout des packets pour gerer la modification du mana

---
+++ src/main/java/ros/eagleoffire/rosevolution/network/packets/AddChakraC2SPacket.java
@@ -0,0 +1,17 @@
+package ros.eagleoffire.rosevolution.network.packets;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+public class AddChakraC2SPacket {
+    public AddChakraC2SPacket(FriendlyByteBuf buf) {
+    }
+
+    public void toBytes(FriendlyByteBuf buf) {
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> contextSupplier) {
+    }
+}
